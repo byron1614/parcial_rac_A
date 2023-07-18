1e0a525fcbf6a4214ea43a5af55b37c5db6b32fd
byron <gemechama11203jorge@gmail.com>
2023-07-18 11:30:24 -0600

    se agrego el codigo para la obtención de elementos del formulario y la tabla para personajes

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Obtención de elementos del formulario y la tabla para personajes
+    const formCharacter = document.getElementById('formCharacter');
+    const inputCharacter = document.getElementById('inputCharacter');
+    const tbodyCharacter = document.getElementById('tbodyCharacter');

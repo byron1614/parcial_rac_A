05c410519d520826654fc5b1c5a17d44fdd87f35
byron <gemechama11203jorge@gmail.com>
2023-07-18 11:33:41 -0600

    se agrego la obtencion de elementos del formulario y la tabla para personajes tambien el Manejador del evento de envío del formulario para buscar personajes

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const formCharacter = document.getElementById('formCharacter');
     const inputCharacter = document.getElementById('inputCharacter');
     const tbodyCharacter = document.getElementById('tbodyCharacter');
+
+    // Manejador del evento de envío del formulario para buscar personajes
+  formCharacter.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const characterName = inputCharacter.value.trim();
+    if (characterName === '') {
+      alert('Por favor, ingresa el nombre del personaje');
+      return;
+    }
+    const url = `https://rickandmortyapi.com/api/character/?name=${characterName}`;
+
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        mostrarPersonajes(data.results);
+      } else {
+        alert('Error ingrese un dato válido');
+      }
+    } catch (error) {
+      alert('Error ingrese lo que se le pide en el campo');
+      console.log(error);
+    }
+  });
